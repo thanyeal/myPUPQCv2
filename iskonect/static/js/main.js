c8737d8cc54eb5070996f012a0ae4baf08a74027
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-06-06 12:22:04 +0800

    style: adjusted styles

--- iskonect/static/js/main.js
+++ iskonect/static/js/main.js
@@ -132,4 +132,15 @@
    */
   new PureCounter();
 
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll('.swiper').forEach(function(swiper) {
+      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
+      new Swiper(swiper, config);
+    });
+  }
+  window.addEventListener('load', initSwiper);
+
 })()
